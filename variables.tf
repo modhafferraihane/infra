24e4a1e1569effcb63d3362e8be84f6779c0b23b
modovar <modhaffer.raihane@hotmail.com>
2025-04-09 19:07:02 +0100

    refactor: remove unused subnet CIDR variables and availability zones

--- variables.tf
+++ variables.tf
@@ -49,23 +49,6 @@ variable "private_subnets" {
     }
   ]
 }
-# variable "public_subnet_cidrs" {
-#   description = "CIDR blocks for public subnets"
-#   type        = list(string)
-#   default     = ["10.0.1.0/24", "10.0.2.0/24"]
-# }
-
-# variable "private_subnet_cidrs" {
-#   description = "CIDR blocks for private subnets"
-#   type        = list(string)
-#   default     = ["10.0.3.0/24", "10.0.4.0/24"]
-# }
-
-# variable "availability_zones" {
-#   description = "List of availability zones"
-#   type        = list(string)
-#   default     = ["us-east-1a", "us-east-1b"]
-# }
 
 variable "common_tags" {
   description = "Common tags for all resources"
